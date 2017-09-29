034efb4e256493a5ea4d489575c511de96bdc571
Aaron J. Smith <onsmith@cs.unc.edu>
2017-09-28 20:58:17 -0400

    Playing with c++

--- FramelessCodecs/Downsampler/Gray16Frame.cpp
+++ FramelessCodecs/Downsampler/Gray16Frame.cpp
@@ -1,27 +1,54 @@
 #include "Gray16Frame.h"
 
 
-
-void Gray16Frame::initialize(const unsigned int width, const unsigned int height, const unsigned int index) {
-	this->width  = width;
-	this->height = height;
-	this->index  = index;
-	this->data   = new uint16_t[width * height];
+Gray16Frame::intensity_t* Gray16Frame::getData() {
+	return data;
 }
 
 
-Gray16Frame::Gray16Frame(const unsigned int width, const unsigned int height, const unsigned int index) {
-	this->width  = width;
-	this->height = height;
-	this->index  = index;
-	this->data   = new uint16_t[width * height];
+Gray16Frame::intensity_t* Gray16Frame::getLuma() {
+	return data;
+}
+
+
+unsigned int Gray16Frame::getNumPixels() {
+	return width * height;
+}
+
+
+size_t Gray16Frame::getDataLength() {
+	return width * height;
+}
+
+
+Gray16Frame::intensity_t Gray16Frame::getIntensity(unsigned int i) {
+	return data[i];
+}
+
+
+Gray16Frame::intensity_t Gray16Frame::getIntensity(unsigned int x, unsigned int y) {
+	return data[y * width + x];
 }
 
 
 Gray16Frame::Gray16Frame(const unsigned int width, const unsigned int height) {
+	this->width  = width;
+	this->height = height;
+	this->data   = new uint16_t[getDataLength()];
+}
 
+
+Gray16Frame::Gray16Frame(Gray16Frame &obj) {
+	const unsigned int dataLength = getDataLength();
+	width  = obj.width;
+	height = obj.height;
+	data   = new uint16_t[dataLength];
+	for (unsigned int i = 0; i < dataLength; i++) {
+		data[i] = obj.getIntensity(i);
+	}
 }
 
 
 Gray16Frame::~Gray16Frame() {
+	delete data;
 }
--- FramelessCodecs/Downsampler/Gray16Frame.h
+++ FramelessCodecs/Downsampler/Gray16Frame.h
@@ -4,29 +4,69 @@
 
 
 class Gray16Frame {
+public:
+	/*
+	** Defines which type an intensity value should be.
+	*/
+	typedef uint16_t intensity_t;
+
+private:
 	/*
 	 ** The width and height of the frame.
 	 */
 	unsigned int width, height;
-	
+
 	/*
-	 ** The index of the frame.
+	 ** The raw array holding the frame data.
 	 */
-	unsigned int index;
-
-	/*
-	 ** The array that holds the frame data.
-	 */
-	uint16_t *data;
-
-	/*
-	** Private constructor function.
-	*/
-	void initialize(const unsigned int, const unsigned int, const unsigned int);
-
+	intensity_t* data;
 
 public:
-	Gray16Frame(const unsigned int, const unsigned int, const unsigned int);
+	/*
+	** Gets the raw array of pixel intensity values.
+	*/
+	intensity_t* getData();
+
+	/*
+	** Gets the raw array of luma pixel intensity values.
+	*/
+	intensity_t* getLuma();
+
+	/*
+	** Gets the total number of pixels in the frame.
+	*/
+	unsigned int getNumPixels();
+
+	/*
+	** Gets the total size of the frame array.
+	*/
+	size_t getDataLength();
+
+	/*
+	** Gets the intensity of the pixel located at position (x,y), where the
+	**   origin is located at the top left corner of the frame, and pixels are
+	**   zero-indexed.
+	*/
+	intensity_t getIntensity(unsigned int x, unsigned int y);
+
+	/*
+	** Gets the intensity of the pixel located at position i, where pixels are
+	**   zero-indexed in row-major order.
+	*/
+	intensity_t getIntensity(unsigned int i);
+
+	/*
+	** Constructor
+	*/
 	Gray16Frame(const unsigned int, const unsigned int);
+
+	/*
+	** Copy constructor
+	*/
+	Gray16Frame(Gray16Frame&);
+
+	/*
+	** Destructor
+	*/
 	~Gray16Frame();
 };
